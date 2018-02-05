9718c5c882ed49684810d4e46e294a8aa9f2cb90
Dracologist <ejckanzler@yahoo.com>
2018-02-05 14:41:26 -0500

    Creating directory structure

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * Users: Elizabeth Kanzler and Jarod Bose
+ * Date: 2/5/2018
+ * Time: 11:39 AM
+ */
+
+<?php
+    require_once('vendor/autoload.php');
+    $f3 = Base::instance();
+
+//    $f3->route('GET /', function() {
+//        echo '<h1>Hello, world!</h1>';
+//    });
+
+    $f3->route('GET /', function() {
+        $view = new View;    echo $view->render
+        ('views/home.html');
+    });
+
+    $f3->run();
