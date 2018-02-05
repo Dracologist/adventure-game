9471ff234e57b5a1bbfa4981105f788c7a055802
Dracologist <ejckanzler@yahoo.com>
2018-02-05 14:57:07 -0500

    Fixing php syntax error

--- index.php
+++ index.php
@@ -1,12 +1,4 @@
 <?php
-/**
- * Created by PhpStorm.
- * Users: Elizabeth Kanzler and Jarod Bose
- * Date: 2/5/2018
- * Time: 11:39 AM
- */
-
-<?php
     require_once('vendor/autoload.php');
     $f3 = Base::instance();
     $f3->route('GET /', function() {
